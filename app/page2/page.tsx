cdfc849e81a00c595d6de21d6f5ca1bc85785747
Ryan Burns <ryan@ryanburns.io>
2025-03-04 10:24:15 -0800

    Add generate metadata with simulated delay

--- app/page2/page.tsx
+++ app/page2/page.tsx
@@ -1,7 +1,22 @@
+import React from "react";
 import Image from "next/image";
 import Link from "next/link";
+import { Metadata } from "next";
 
-export default function Home() {
+export async function generateMetadata(): Promise<Metadata> {
+  // Simulate a delay in generating metadata
+  await new Promise((resolve) => setTimeout(resolve, 1500));
+
+  return {
+    title: "Streaming Metadata Bug | Page 2",
+    description: "A bug in Next.js canary",
+    icons: {
+      icon: "/star.png",
+    },
+  };
+}
+
+export default function Page() {
   return (
     <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
       <main className="flex flex-col gap-8 row-start-2 items-center sm:items-start">
@@ -17,9 +32,9 @@ export default function Home() {
         <div className="flex gap-4 items-center flex-col sm:flex-row">
           <Link
             className="rounded-full border border-solid border-black/[.08] dark:border-white/[.145] transition-colors flex items-center justify-center hover:bg-[#f2f2f2] dark:hover:bg-[#1a1a1a] hover:border-transparent text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 sm:min-w-44"
-            href="/page2"
+            href="/"
           >
-            Go to page 2
+            Go back home
           </Link>
         </div>
       </main>
